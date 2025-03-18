088be12178a9bd1875b898bbaafde249e74ac013
nae_bananes <Nayeong.Kim.Ansan@gmail.com>
2025-03-18 16:29:46 +0800

    minor change

--- 1_Processing/0_HK weather data aggregation.py
+++ 1_Processing/0_HK weather data aggregation.py
@@ -10,26 +10,37 @@ import glob
 
 # CSV File list in 0_Data directory:
 list_files = glob.glob("0_Data/*.csv")
-
+len(list_files)   
 # Aggregated Data templete:
 Agg_Data = pd.DataFrame()
-#columns = ['Date','year','Temp','Flag']
+Data_summary = pd.DataFrame()
+
 for Data_by_dist in list_files:
     
     Max_region=pd.read_csv(Data_by_dist, 
                        sep=",", skiprows=3, header = None)
 
     LMMAXT_HKP_ = pd.DataFrame(Max_region)
-    LMMAXT_HKP_.columns = ['year', 'month', 'day', 'Temp', 'Flag']
+    LMMAXT_HKP_.columns = ['year', 'month', 'day', 'value', 'Flag']
 
+    # Data Quality indicators:
+    Obs_summary = pd.DataFrame({'Dist': [Data_by_dist.split("Data/")[1].split("_")[1]],
+                                'NObs_Dist': [LMMAXT_HKP_.shape[0]],
+                                'NObs_Flagged': [LMMAXT_HKP_.loc[LMMAXT_HKP_['Flag']=="#"].shape[0]],
+                                'NObs_Starred': [LMMAXT_HKP_.loc[LMMAXT_HKP_['value']=="***"].shape[0]],
+                                'NObs_Str_Shrp': [LMMAXT_HKP_.loc[(LMMAXT_HKP_['value']=="***")&(LMMAXT_HKP_['Flag']=="#")].shape[0]]})
+   
+    Obs_summary['Purge_Pct'] = round(100*Obs_summary['NObs_Starred']/Obs_summary['NObs_Dist'],2)
+    Data_summary = pd.concat([Data_summary, Obs_summary], axis = 0)
+    
     ## Data Filter ##
     # F1: Delete the last 3 lines:
     LMMAXT_HKP_ = LMMAXT_HKP_.iloc[:-3]
 
     LMMAXT_HKP_['month'] = LMMAXT_HKP_['month'].astype(int)
     LMMAXT_HKP_['day'] = LMMAXT_HKP_['day'].astype(int)
-    LMMAXT_HKP_ = LMMAXT_HKP_.loc[LMMAXT_HKP_['Temp']!="***"]
-    LMMAXT_HKP_['Temp'] = LMMAXT_HKP_['Temp'].astype(float)
+    LMMAXT_HKP_ = LMMAXT_HKP_.loc[LMMAXT_HKP_['value']!="***"]
+    LMMAXT_HKP_['value'] = LMMAXT_HKP_['value'].astype(float)
 
     # F2: Delete data of 2007 since incomplete (only from october)
     LMMAXT_HKP_ = LMMAXT_HKP_.loc[LMMAXT_HKP_['year']!='2007']
@@ -39,54 +50,16 @@ for Data_by_dist in list_files:
     #LMMAXT_HKP_date = LMMAXT_HKP_date.dt.dayofyear
 
     # Concatenate parsed date + Table 
-    LMMAXT_HKP_2 = pd.concat([LMMAXT_HKP_date, LMMAXT_HKP_[['year','Temp', 'Flag']]],
+    LMMAXT_HKP_2 = pd.concat([LMMAXT_HKP_date, LMMAXT_HKP_[['year','value', 'Flag']]],
             axis = 1)
     LMMAXT_HKP_2 = LMMAXT_HKP_2.rename(columns = {0: "Date"})
-    
-    # Observatory center name
-    LMMAXT_HKP_2['Dist'] = Data_by_dist.split("Data/")[1].split("_")[1]
-    LMMAXT_HKP_2['NObs_Dist'] = LMMAXT_HKP_2.shape[0]
     
     Agg_Data = pd.concat([Agg_Data, LMMAXT_HKP_2], axis = 0, ignore_index=True)
 
 
-# Plot : Max temperature measured at HKP
+Data_summary.loc[Data_summary['Purge_Pct']<1].shape[0]/Data_summary.shape[0]
 
-# (1) Monthly Average Max temperature
-
-LMMAXT_HKP_monthlyMean = LMMAXT_HKP_.groupby(['year', 'month'])['Temp'].mean()
-LMMAXT_HKP_monthlyMean = pd.DataFrame(LMMAXT_HKP_monthlyMean.reset_index())
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-sns.set_theme(style="dark")
-
-# Plot each year's time series in its own facet
-g = sns.relplot(
-    data=LMMAXT_HKP_monthlyMean,
-    x="month", y="Temp", col="year", hue="year",
-    kind="line", palette="crest", linewidth=2, zorder=5,
-    col_wrap=3, height=2, aspect=1.5, legend=False,
-)
-
-# Iterate over each subplot to customize further
-for year, ax in g.axes_dict.items():
-
-    # Add the title as an annotation within the plot
-    ax.text(.8, .85, year, transform=ax.transAxes, fontweight="bold")
-
-    # Plot every year's time series in the background
-    sns.lineplot(
-        data=LMMAXT_HKP_monthlyMean, x="month", y="Temp", units="year",
-        estimator=None, color=".7", linewidth=1, ax=ax,
-    )
-
-# Reduce the frequency of the x axis ticks
-ax.set_xticks(ax.get_xticks()[::2])
-
-# Tweak the supporting aspects of the plot
-g.set_titles("")
-g.set_axis_labels("", "")
-g.tight_layout()
-plt.show()
+treated_data_rep = r'0_Data/wrangled/' 
+if not os.path.exists(treated_data_rep):
+    os.makedirs(treated_data_rep)
+Agg_Data.to_pickle(treated_data_rep+"Agg_Data.pkl")
